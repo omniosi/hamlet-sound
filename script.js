40b628f12845ac41d0f7f0bdcb0ac0ccd74ef600
Ornette Coleman <omniosi@gmail.com>
2018-09-27 23:52:22 -0700

    PLAN: added js file, chnaged folder name,  add placeholders

---
+++ script.js
@@ -0,0 +1,5 @@
+// each button click triggers invisible audio file play once
+
+// video on complete event triggers function to replace current video source with next in line
+
+// when correct audio selected, next video load and replace. current button disabled
